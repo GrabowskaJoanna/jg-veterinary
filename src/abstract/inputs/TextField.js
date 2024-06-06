0b38e106db9a481857d663ea27460f3d58915222
GrabowskaJoanna <joanna.trojanek18@gmail.com>
2024-06-06 15:12:28 +0200

    Added state to loginForm

--- src/abstract/inputs/TextField.js
+++ src/abstract/inputs/TextField.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const TextField = ({type, name, text}) => {
-
-    return (
-        <label>{text}
-            <input type={type} name={name}/>
-        </label>
-    );
+const TextField = ({ type, name, text, value, onChange }) => {
+  return (
+    <label>
+      {text}
+      <input type={type} name={name} value={value} onChange={onChange} />
+    </label>
+  );
 };
 
 export default TextField;
